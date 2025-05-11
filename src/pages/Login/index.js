a6c00a11ee4c4b957908a12457915d6885fb5336
null0827 <1476785005@qq.com>
2025-05-12 02:14:35 +0800

    build: 表单校验和数据收集

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,43 @@
+import "./index.scss";
+import { Card, Form, Input, Button } from "antd";
+import logo from "@/assets/logo.png";
+
 const Login = () => {
-  return <div>this is Login</div>;
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        <Form onFinish={onFinish} validateTrigger="onBlur">
+          <Form.Item
+            name="username"
+            rules={[
+              { required: true, message: "Please input your username!" },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: "请输入正确的手机号",
+              },
+            ]}
+          >
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 };
 
 export default Login;
