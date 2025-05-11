10474e42a04925590a24d3173df6f2a2dfdfa083
null0827 <1476785005@qq.com>
2025-05-12 02:30:49 +0800

    build: axios封装

---
+++ src/utils/request.js
@@ -0,0 +1,32 @@
+//axios的封装处理
+import axios from "axios";
+//1.根域名配置
+//2.超时时间
+//3.请求拦截器/响应拦截器
+
+const request = axios.create({
+  baseURL: "http://hjj.xiecheng.net/v1_0",
+  timeout: 5000,
+});
+
+//添加请求拦截器
+request.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+//添加响应拦截器
+request.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export { request };
